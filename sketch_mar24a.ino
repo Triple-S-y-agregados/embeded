2de2f0858891d284af9a1975df6f03d252e18595
Salva <salva.hg01@gmail.com>
2021-03-24 19:45:44 -0600

    Minor adjustments

--- sketch_mar24a.ino
+++ sketch_mar24a.ino
@@ -10,38 +10,75 @@
 /*Definimos los objetos tipo Servo*/
 //Para el servo con movimiento horizontal
 Servo servoH; //Objeto tipo Servo.
-int servoh = 0;
-int servohLimitHigh = 160;
-int servohLimitLow = 20;
-
 //Para el servo con movimiento vertical
 Servo servoV; //Objeto tipo Servo.
-int servov = 0; 
-int servovLimitHigh = 160;
-int servovLimitLow = 20;
 
-//Creción e inicialización de variables  para los LDRs
-int ldrtopl = 2; //LDR arriba a la izquierda.
-int ldrtopr = 1; //LDR arriba a la derecha.
-int ldrbotl = 3; //LDR abajo a la izquierda.
-int ldrbotr = 0; //LDR abajo a la derecha
+#define HORIZONTAL_SERVO_PIN 10
+#define VERTICAL_SERVO_PIN 9
+
+#define SERVO_HIGH_LIMIT 160
+#define SERVO_LOW_LIMIT 20
+
+#define INITIAL_POSITION 0
+
+#define TOP_RIGHT_LDR 1
+#define TOP_LEFT_LDR 2
+#define BOTTOM_RIGHT_LDR 0
+#define BOTTOM_LEFT_LDR 3
+
+void move_right() {
+  int servo_horizontal_position = servoH.read();
+  int next_position = servo_horizontal_position + 1;
+
+  if (servoh > SERVO_HIGH_LIMIT)
+    servoh = SERVO_HIGH_LIMIT;
+
+  servoH.write(next_position);
+}
+
+void move_left() {
+  int servo_horizontal_position = servoH.read();
+  int next_position = servo_horizontal_position - 1;
+
+  if (next_position < SERVO_LOW_LIMIT)
+    next_position = SERVO_LOW_LIMIT;
+  
+  servoH.write(next_position);
+}
+
+void move_up() {
+  int servo_vertical_position = servoV.read();
+  int next_position = servo_vertical_position + 1;
+
+  if (servov > SERVO_HIGH_LIMIT)
+    servov = SERVO_HIGH_LIMIT;
+
+  servoV.write(next_position);
+}
+
+void move_down() {
+  int servo_vertical_position = servoV.read();
+  int next_position = servo_vertical_position - 1;
+  
+  if (servov < SERVO_LOW_LIMIT)
+    servov = SERVO_LOW_LIMIT;
+
+  servoV.write(next_position);
+}
 
 void setup () {
-  servoH.attach(10);
-  servoH.write(0);
-  servoV.attach(9);
-  servoV.write(0);
-  delay(500); //Espera de 500ms.
+  servoH.attach(HORIZONTAL_SERVO_PIN);
+  servoH.write(INITIAL_POSITION);
+  servoV.attach(VERTICAL_SERVO_PIN);
+  servoV.write(INITIAL_POSITION);
 }
 
 void loop() {
-  servoh = servoH.read();
-  servov = servoV.read();
   //capturando valores analogicos de cada LDR
-  int topl = analogRead(ldrtopl);
-  int topr = analogRead(ldrtopr);
-  int botl = analogRead(ldrbotl);
-  int botr = analogRead(ldrbotr);
+  int topl = analogRead(TOP_LEFT_LDR);
+  int topr = analogRead(TOP_RIGHT_LDR);
+  int botl = analogRead(BOTTOM_LEFT_LDR);
+  int botr = analogRead(BOTTOM_RIGHT_LDR);
   // calculando el Promedio
   int avgtop = (topl + topr) / 2; //Promedio del top LDRs
   int avgbot = (botl + botr) / 2; //Promedio del bottom LDRs
@@ -49,39 +86,18 @@ void loop() {
   int avgright = (topr + botr) / 2; //Promedio del right LDRs
 
   if (avgtop < avgbot) {
-    servoV.write(servov +1);
-    if (servov > servovLimitHigh) { 
-      servov = servovLimitHigh;
-    }
-    delay(10);
+    move_up();
   }
   else if (avgbot < avgtop) {
-    servoV.write(servov -1);
-    if (servov < servovLimitLow) {
-      servov = servovLimitLow;
-    }
-    delay(10);
+    move_down();
   }
-  else {
-    servoV.write(servov);
-  }
+  delay(50);
   
   if (avgleft > avgright) {
-    servoH.write(servoh +1);
-    if (servoh > servohLimitHigh) {
-      servoh = servohLimitHigh;
-    }
-    delay(10);
+    move_right();
   }
   else if (avgright > avgleft) {
-    servoH.write(servoh -1);
-    if (servoh < servohLimitLow) {
-     servoh = servohLimitLow;
-    }
-    delay(10);
-  }
-  else {
-    servoH.write(servoh);
+    move_left();
   }
   delay(50);
 }
