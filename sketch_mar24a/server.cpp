b8de6da26872461b11bb94568b687b553f09fb7d
Salva09 <salva.hg01@gmail.com>
2021-05-03 16:40:18 -0500

    Add thread template for server

---
+++ sketch_mar24a/server.cpp
@@ -0,0 +1,20 @@
+#include "server.h"
+
+TaskHandle_t Task1;
+
+void initialize_server() {
+    xTaskCreatePinnedToCore(
+      create_server, /* Function to implement the task */
+      "Server", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      0,  /* Priority of the task */
+      &Task1,  /* Task handle. */
+      1); /* Core where the task should run */
+}
+
+void create_server() {}
+
+void delete_server() {
+    vTaskDelete(Task1);
+}
---
+++ sketch_mar24a/server.h
@@ -0,0 +1,8 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+void initialize_server();
+void create_server();
+void delete_server();
+
+#endif
