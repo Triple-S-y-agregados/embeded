447c544f87deb44c5dea40ff07cb760382bb2c1c
Salva <salva.hg01@gmail.com>
2021-05-07 20:35:02 -0500

    Fix identation

--- main/network.cpp
+++ main/network.cpp
@@ -12,11 +12,11 @@ void connect_wifi() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("");
+  Serial.println();
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
