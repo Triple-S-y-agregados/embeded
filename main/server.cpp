da8f4b51b372264b52282b1280c7958a619d43f8
Salva <salva.hg01@gmail.com>
2021-05-07 20:58:03 -0500

    Remove unnecessary comments

--- main/server.cpp
+++ main/server.cpp
@@ -3,14 +3,7 @@
 TaskHandle_t Task1;
 
 void initialize_server() {
-    xTaskCreatePinnedToCore(
-      create_server, /* Function to implement the task */
-      "Server", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &Task1,  /* Task handle. */
-      1); /* Core where the task should run */
+    xTaskCreatePinnedToCore(create_server, "Server", 10000, NULL, 0, &Task1, 1);
 }
 
 void create_server(void* this_parameter_is_completely_irrelevant_so_dont_use_it) {}
