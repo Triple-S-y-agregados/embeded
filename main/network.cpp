fb13eebaad41c1328f400f9e679eddce88451c17
Salva HG <salva.hg01@gmail.com>
2021-05-13 21:53:44 -0500

    Move constants out of the header file

--- main/network.cpp
+++ main/network.cpp
@@ -1,5 +1,12 @@
 #include "network.h"
 
+const char* ssid = "your_ssid";
+const char* pwd = "your_password";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+
 void connect_wifi() {
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
