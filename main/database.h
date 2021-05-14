9e122c006448c6e2adcbc7765fa9bb50f52a49fc
Salva HG <salva.hg01@gmail.com>
2021-05-13 19:39:55 -0500

    Add analog data logging

--- main/database.h
+++ main/database.h
@@ -6,12 +6,27 @@
 #include <FS.h>
 #include "SPIFFS.h"
 #include "SD_MMC.h"
-#include "SD.h"
+#include <SD.h>
+#include <sd_diskio.h>
+#include <sd_defines.h>
 
 #define FORMAT_SPIFFS_IF_FAILED true
-#define MAX_FILE_NAME_LEN 100
 #define MAX_STR_LEN 500
 
 #define BUF_SIZE 4096
+byte buf[BUF_SIZE];
+const char* database_name = "solar_db";
+extern const char sqlite_sig[];
+
+FILE *myFile;
+
+void initialize_database();
+void log_data();
+void get_record();
+
+int32_t read_fn_wctx(struct dblog_write_context*, void*, uint32_t, size_t);
+int flush_fn(struct dblog_write_context*);
+int32_t write_fn(struct dblog_write_context*, void*, uint32_t, size_t);
+void print_error(int);
 
 #endif
