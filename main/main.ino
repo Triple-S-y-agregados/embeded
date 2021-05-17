e81d9770478ce8a9ba5c8d4fdac38e41fe2c46c4
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2021-05-17 05:34:11 -0500

    postVoltage function

--- main/main.ino
+++ main/main.ino
@@ -10,17 +10,14 @@
 Servo servoH;
 Servo servoV;
 
-
 void setup () {
   Serial.begin( 115200 );
   initialize_servos( servoH, servoV );
   connect_wifi();
-  initialize_client();
 }
 
 void loop() {
-  float V_pin = map(analogRead( PANEL_PIN ), 0, 4095, 0, 3); // Puede que sea necesario cambiar el voltaje máximo
-  float V_panel = 4 * V_pin;
   update_servos( servoH, servoV );
   postVoltage();
+  delay(1000);
 }
