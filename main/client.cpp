7f8f487e22d840e13d803f96cebdabaa21dae6c1
Andrik-Ts-019 <andrikdelacruz@gmail.com>
2021-05-17 05:36:35 -0500

    free buffer

--- main/client.cpp
+++ main/client.cpp
@@ -6,21 +6,20 @@ StaticJsonDocument<200> doc;
 String buffer = "";
 String ServerIP = "http://192.168.0.3:44388/";
 
-
-void initialize_client() {
-  // xTaskCreatePinnedToCore(create_server, "Server", 10000, NULL, 0, &Task1, 1);
-}
-
 // voltage
 void postVoltage() {
   http.begin(ServerIP + "voltage");
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.addHeader("Content-Type", "application/json");
   String requestData = "";
   float V_pin = map(analogRead( PANEL_PIN ), 0, 4095, 0, 3); // Puede que sea necesario cambiar el voltaje máximo
   float V_panel = 4 * V_pin;
   doc["voltage"] = V_panel;
   serializeJson( doc, buffer );
+
+  Serial.println(buffer);
+  
   int responseCode = http.POST( buffer );
+  doc.clear();
   Serial.println("Código de salida: " + responseCode);
 }
 
